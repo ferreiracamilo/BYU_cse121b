96f11911c33c3623168d0cf5e79ead7eec9a1d26
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-07-09 12:52:47 -0300

    update file

--- week6/final_project/scripts/task6.js
+++ week6/final_project/scripts/task6.js
@@ -6,8 +6,15 @@ let playersStats = [];
 
 function defaultContent(){
     for(let i = 1; i <=5; i++){
+        
         player_img = document.querySelector('#player1-image');
-        player_nickname = document.querySelector('#player1-image');
+        player_nickname = document.querySelector('#player1-nickname');
+        player_fullname = document.querySelector('#player1-fullname');
+        player_maps_played = document.querySelector('#player1-maps-played');
+        player_kd = document.querySelector('#player1-kd');
+        player_rating = document.querySelector('#player1-rating');
+        player_country = document.querySelector('#player1-country');
+        player_country_img = document.querySelector('#player1-country-img');
         //player1-image
         //player1-nickname
         //player1-fullname
