5fb2a85ebe7de3867fd61c3f3bfe2a00910828aa
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-07-09 12:32:16 -0300

    push update to footer

--- week6/final_project/scripts/task6.js
+++ week6/final_project/scripts/task6.js
@@ -1,2 +1,6 @@
 default_noimg_player = "https://www.hltv.org/img/static/player/player_silhouette.png"
 defaul_noimg_country = "https://www.hltv.org/img/static/flags/30x20/WORLD.gif"
+
+let playersInfo = [];
+let playersStats = [];
+
