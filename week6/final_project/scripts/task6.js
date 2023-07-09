53b2b7698754ec0263321eaf4bffffe0c4cd7681
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-07-09 12:49:26 -0300

    pushchanges

--- week6/final_project/scripts/task6.js
+++ week6/final_project/scripts/task6.js
@@ -4,3 +4,19 @@ defaul_noimg_country = "https://www.hltv.org/img/static/flags/30x20/WORLD.gif"
 let playersInfo = [];
 let playersStats = [];
 
+function defaultContent(){
+    for(let i = 1; i <=5; i++){
+        player_img = document.querySelector('#player1-image');
+        player_nickname = document.querySelector('#player1-image');
+        //player1-image
+        //player1-nickname
+        //player1-fullname
+        //player1-maps-played
+        //player1-kd
+        //player1-rating
+        //player1-country
+        //player1-country-img
+    }
+    
+    //document.querySelector('#message2').textContent = msgToDisplay2;
+}
