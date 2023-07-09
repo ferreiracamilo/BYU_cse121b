618315040be89ba1dc2d45a8bc61adb11be406ff
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-07-09 13:00:57 -0300

    push changes

--- week6/final_project/scripts/task6.js
+++ week6/final_project/scripts/task6.js
@@ -5,16 +5,24 @@ let playersInfo = [];
 let playersStats = [];
 
 function defaultContent(){
-    for(let i = 1; i <=5; i++){
-        
-        player_img = document.querySelector('#player1-image');
-        player_nickname = document.querySelector('#player1-nickname');
-        player_fullname = document.querySelector('#player1-fullname');
-        player_maps_played = document.querySelector('#player1-maps-played');
-        player_kd = document.querySelector('#player1-kd');
-        player_rating = document.querySelector('#player1-rating');
-        player_country = document.querySelector('#player1-country');
-        player_country_img = document.querySelector('#player1-country-img');
+    for(let i = 1; i <=5; i++){   
+        player_img = document.querySelector('#player'+ i +'-image');
+        player_nickname = document.querySelector('#player'+ i +'-nickname');
+        player_fullname = document.querySelector('#player'+ i +'-fullname');
+        player_maps_played = document.querySelector('#player'+ i +'-maps-played');
+        player_kd = document.querySelector('#player'+ i +'-kd');
+        player_rating = document.querySelector('#player'+ i +'-rating');
+        player_country = document.querySelector('#player'+ i +'-country');
+        player_country_img = document.querySelector('#player'+ i +'-country-img');
+
+        player_img.setAttribute("src","images/default_flag.png");
+        player_nickname.textContent = "Placeholder " + i;
+        player_fullname.textContent = "Lorem Ipsum";
+        player_maps_played.textContent = "Lorem Ipsum";
+        player_kd.textContent = "Lorem Ipsum";
+        player_rating.textContent = "Lorem Ipsum";
+        player_country.textContent = "Lorem Ipsum";
+        player_country_img.setAttribute("src","images/default_flag.png");
         //player1-image
         //player1-nickname
         //player1-fullname
@@ -25,5 +33,5 @@ function defaultContent(){
         //player1-country-img
     }
     
-    //document.querySelector('#message2').textContent = msgToDisplay2;
+    defaultContent();
 }
