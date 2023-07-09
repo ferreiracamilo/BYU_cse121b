e7a2afb2dbe498ba75eed5fef7358938944e890d
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-07-09 20:46:43 -0300

    push changes

--- week6/final_project/scripts/task6.js
+++ week6/final_project/scripts/task6.js
@@ -5,33 +5,56 @@ let playersInfo = [];
 let playersStats = [];
 
 function defaultContent(){
-    for(let i = 1; i <=5; i++){   
-        player_img = document.querySelector('#player'+ i +'-image');
-        player_nickname = document.querySelector('#player'+ i +'-nickname');
-        player_fullname = document.querySelector('#player'+ i +'-fullname');
-        player_maps_played = document.querySelector('#player'+ i +'-maps-played');
-        player_kd = document.querySelector('#player'+ i +'-kd');
-        player_rating = document.querySelector('#player'+ i +'-rating');
-        player_country = document.querySelector('#player'+ i +'-country');
-        player_country_img = document.querySelector('#player'+ i +'-country-img');
-
-        player_img.setAttribute("src","images/default_flag.png");
-        player_nickname.textContent = "Placeholder " + i;
-        player_fullname.textContent = "Lorem Ipsum";
-        player_maps_played.textContent = "Lorem Ipsum";
-        player_kd.textContent = "Lorem Ipsum";
-        player_rating.textContent = "Lorem Ipsum";
-        player_country.textContent = "Lorem Ipsum";
-        player_country_img.setAttribute("src","images/default_flag.png");
-        //player1-image
-        //player1-nickname
-        //player1-fullname
-        //player1-maps-played
-        //player1-kd
-        //player1-rating
-        //player1-country
-        //player1-country-img
+    for(let i=1; i<=5; i++){ 
+        setContentPlayer(i, "images/default_player.png", "Placeholder " + i, "Lorem Ipsum", 0, 0.0, 0, "Lorem Ipsum", "images/default_flag.png");
     }
-    
-    defaultContent();
 }
+
+function setContentPlayer(id, playerImageURL, nickname, fullname, maps_played, kd, rating, country, countryImageURL){
+    player_img = document.querySelector('#player'+ id +'-image');
+    player_nickname = document.querySelector('#player'+ id +'-nickname');
+    player_fullname = document.querySelector('#player'+ id +'-fullname');
+    player_maps_played = document.querySelector('#player'+ id +'-maps-played');
+    player_kd = document.querySelector('#player'+ id +'-kd');
+    player_rating = document.querySelector('#player'+ id +'-rating');
+    player_country = document.querySelector('#player'+ id +'-country');
+    player_country_img = document.querySelector('#player'+ id +'-country-img');
+
+    player_img.setAttribute("src", playerImageURL);
+    player_nickname.textContent = nickname;
+    player_fullname.textContent = fullname;
+    player_maps_played.textContent = maps_played;
+    player_kd.textContent = kd;
+    player_rating.textContent = rating;
+    player_country.textContent = country;
+    player_country_img.setAttribute("src",countryImageURL);  
+}
+
+// Function to check if the target elements are present
+function checkElements() {
+    return new Promise((resolve) => {
+      const targetElements = document.querySelectorAll('article');
+      if (targetElements.length === 5) {
+        resolve(targetElements);
+      } else {
+        const observer = new MutationObserver(() => {
+          const updatedTargetElements = document.querySelectorAll('article');
+          if (updatedTargetElements.length === 5) {
+            observer.disconnect();
+            resolve(updatedTargetElements);
+          }
+        });
+        observer.observe(document.body, { childList: true, subtree: true });
+      }
+    });
+}
+
+// Usage
+(async () => {
+    try {
+      const elements = await checkElements();
+      defaultContent();
+    } catch (error) {
+      console.error("Error:", error);
+    }
+  })();
