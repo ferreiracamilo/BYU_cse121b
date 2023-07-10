e1b09e34be9a83427eb50b1b1b9ec5b3d4c353c3
ferreiracamilo <camiloferreira2009@hotmail.com>
2023-07-09 23:00:19 -0300

    Clean, organize and improve code
    Split into functions for better reading and undestandability

--- week6/final_project/scripts/main.js
+++ week6/final_project/scripts/main.js
@@ -21,49 +21,60 @@ fetch('https://hltv-api.vercel.app/api/player.json')
 
 // Show team data in carousel
 function showTeam(index) {
+    //Based on index retrieve which team to work with
     const team = teams[index];
+
+    //Update Logo and Team Name
     teamLogo.src = team.logo;
-
-    // Clear existing player cards
-    playerCard.innerHTML = '';
-
     teamname.textContent = team.name;
 
-    // Create player cards for each player
-    team.players.forEach(player => {
-        const card = document.createElement('article');
-        card.classList.add('player-card');
+    //Clear existing player cards
+    playerCard.innerHTML = '';
 
-        const playerImage = document.createElement('img');
-        playerImage.src = player.image;
-        playerImage.alt = player.nickname + ' Profile Image';
-
-        const playerNickName = document.createElement('h3');
-        playerNickName.textContent = player.nickname;
-
-        const playerFullName = document.createElement('p');
-        //Full Name includes nickname, so needs a treatment for removal
-        nickNamePattern = "'"+ player.nickname + "'";
-        cleanFullName = player.fullname.replace(nickNamePattern,"");
-        playerFullName.textContent = "Full name: " + cleanFullName;
-
-        const playerCountry = document.createElement('p');
-        playerCountry.textContent = "Country: " + player.country.name;
-
-        const countryImage = document.createElement('img');
-        countryImage.classList.add('countryflag');
-        countryImage.src = player.country.flag;
-        countryImage.alt = player.nickname + ' Country Flag Image';
-
-        card.appendChild(playerImage);
-        card.appendChild(playerNickName);
-        card.appendChild(playerFullName);
-        card.appendChild(playerCountry);
-        card.appendChild(countryImage);
-
-        sectionCards.appendChild(card);
-    });
+    //Create player cards for each player from team picked
+    team.players.forEach(player => createPlayerCard(player));
 }
+
+function createPlayerCard(player){
+    //Create article to contain player information and add required class
+    const card = document.createElement('article');
+    card.classList.add('player-card');
+
+    //Create image element besides assigning src and alt for accessibility scope
+    const playerImage = document.createElement('img');
+    playerImage.src = player.image;
+    playerImage.alt = player.nickname + ' Profile Image';
+
+    const playerNickName = document.createElement('h3');
+    playerNickName.textContent = player.nickname;
+
+    const playerFullName = document.createElement('p');
+    playerFullName.textContent = "Full name: " + removeTextBetweenQuotes(player.fullname);
+
+    const playerCountry = document.createElement('p');
+    playerCountry.textContent = "Country: " + player.country.name;
+
+    //Create image element besides assigning required class, src and alt for accessibility scope
+    const countryImage = document.createElement('img');
+    countryImage.classList.add('countryflag');
+    countryImage.src = player.country.flag;
+    countryImage.alt = player.nickname + ' Country Flag Image';
+
+    //Append subelements into new player card
+    card.appendChild(playerImage);
+    card.appendChild(playerNickName);
+    card.appendChild(playerFullName);
+    card.appendChild(playerCountry);
+    card.appendChild(countryImage);
+
+    //Append new card to players section
+    sectionCards.appendChild(card);
+}
+
+function removeTextBetweenQuotes(str) {
+    //Full Name includes nickname, so needs a treatment for removal
+    return str.replace(/'.*?'/g, '');
+  }
 
 // Event listeners for navigation buttons
 prevButton.addEventListener('click', () => {
